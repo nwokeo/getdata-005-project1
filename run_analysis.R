d763955a5f212fc9d9d5c8fc23b480f7e5ffa1c3
nwokeo <obiike.nwoke@gmail.com>
2014-07-24 12:29:44 -0700

    Remove tests, alternative methods

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 #by O Nwoke
 require(data.table)
+library(reshape2)
 
 #read files.
 test_data <- read.table(file.path(getwd(), "test/X_test.txt")) #test data
@@ -36,29 +37,18 @@ measurement_labels <- read.table(file.path(getwd(), "features.txt")) #measuremen
     setnames(all_subjects,"V1","Subject")                           #set subject col name
 
     all_labels <- merge(all_labels,activity_labels, all=TRUE, by="Label" ) #add text to label df
-    #all_data$LabelText <- all_labels$LabelText   #equivalent
     all_data <-cbind(all_labels$LabelText,all_data)  #merge label into data document
     all_data <-cbind(all_subjects$Subject,all_data)  #merge subject into data document
 
     colnames(all_data)[1:2] <- c("Subject", "LabelText")
 
 #5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-    library(reshape2)
-
     meanCols <- grep("*mean*",colnames(all_data))
     stdCols <- grep("*std*",colnames(all_data))
     keepCols <- c(meanCols,stdCols)
     melt_data <- melt(all_data, id=c("Subject", "LabelText"), measure.vars=colnames(all_data[keepCols])) 
     
-    ##thin
-    dt <- data.table(melt_data) #create data table
-    #dt_summ <- dt[, mean(value), by = c("Subject", "LabelText", "variable")] #aggregate
-
-    ##fat
-    dt2 <- data.table(all_data)
-    dt_summ2 <- dt2[, lapply(.SD, mean, na.rm=TRUE), by=list(Subject,LabelText) ]
-
-#test
-mean(dt[dt$Subject == 2 & dt$LabelText == "WALKING" & dt$variable == "tBodyAcc_mean_X"]$value)
+    dt <- data.table(all_data)
+    dt_summ <- dt[, lapply(.SD, mean, na.rm=TRUE), by=list(Subject,LabelText) ]
 
 
